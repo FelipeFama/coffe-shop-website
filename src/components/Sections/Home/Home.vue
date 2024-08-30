c87f43560ea5bc469aa598b7551e6d2c08d77fc0
FelipeFama <lipehfama@gmail.com>
2024-08-30 15:11:38 -0300

    feat: add home component

---
+++ src/components/Sections/Home/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="home" id="home">
+    <article class="content">
+      <h3>fresh coffee in the morning</h3>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto ducimus
+        eius ea mollitia ipsum quae!
+      </p>
+    </article>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+@import "./Home.module.scss";
+</style>
