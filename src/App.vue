ffaff8a20324293f5c4deef77476e7cbe0384490
FelipeFama <lipehfama@gmail.com>
2024-08-30 13:19:24 -0300

    feat: add footer component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import Header from "./components/Layout/Header/Header.vue";
+import Footer from "./components/Layout/Footer/Footer.vue";
 </script>
 
 <template>
   <Header />
+  <Footer />
 </template>
---
+++ src/components/Layout/Footer/Footer.vue
@@ -0,0 +1,33 @@
+<template>
+  <footer class="footer">
+    <aside class="share">
+      <a href="#">
+        <FaBandsFacebookF />
+      </a>
+      <a href="#">
+        <FaBandsTwitter />
+      </a>
+      <a href="#">
+        <FaBandsInstagram />
+      </a>
+      <a href="#">
+        <FaBandsLinkedin />
+      </a>
+      <a href="#">
+        <FaBandsPinterest />
+      </a>
+    </aside>
+  </footer>
+</template>
+
+<script setup lang="ts">
+import { FaBandsFacebookF } from "@kalimahapps/vue-icons";
+import { FaBandsTwitter } from "@kalimahapps/vue-icons";
+import { FaBandsInstagram } from "@kalimahapps/vue-icons";
+import { FaBandsLinkedin } from "@kalimahapps/vue-icons";
+import { FaBandsPinterest } from "@kalimahapps/vue-icons";
+</script>
+
+<style lang="scss" scoped>
+@import "./Footer.module.scss";
+</style>
