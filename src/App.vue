94427f18cc4fd24b37a83592fb5a77b02f4b814c
FelipeFama <lipehfama@gmail.com>
2024-08-30 18:48:55 -0300

    feat: add about component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import Header from "./components/Layout/Header/Header.vue";
 import Home from "./components/Sections/Home/Home.vue";
+import About from "./components/Sections/About/About.vue";
 import Footer from "./components/Layout/Footer/Footer.vue";
 </script>
 
 <template>
   <Header />
   <Home />
+  <About />
   <Footer />
 </template>
---
+++ src/components/Sections/About/About.vue
@@ -0,0 +1,29 @@
+<template>
+  <!-- about section -->
+  <section class="about" id="about">
+    <h1 class="heading"><span>about</span> us</h1>
+    <div class="row">
+      <figure class="image">
+        <img src="/src/assets/images/about-img.jpeg" alt="about" />
+      </figure>
+      <article class="content">
+        <h3>what makes our coffee special?</h3>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est, quo
+          ullam? Fugit animi, omnis totam reiciendis neque voluptatibus quis
+          temporibus iste blanditiis unde, ipsam debitis in voluptatem modi
+          veritatis recusandae.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+          inventore nisi tempora consequuntur corporis accusantium veritatis
+          consequatur dolorum illum culpa?
+        </p>
+      </article>
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+@import "./About.module.scss";
+</style>
