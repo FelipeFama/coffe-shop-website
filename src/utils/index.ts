e3d24bbcec286ef8565621a55aeb7bd01d13db31
FelipeFama <lipehfama@gmail.com>
2024-08-31 02:07:11 -0300

    feat: add utils file

---
+++ src/utils/index.ts
@@ -0,0 +1,45 @@
+// utils.ts
+export const menuItems = [
+  {
+    image: "/src/assets/images/menu-1.png",
+    title: "tasty and healthy",
+    price: "$15.99",
+    oldPrice: "$20.99",
+    buttonText: "add to cart",
+  },
+  {
+    image: "/src/assets/images/menu-2.png",
+    title: "tasty and healthy",
+    price: "$15.99",
+    oldPrice: "$20.99",
+    buttonText: "add to cart",
+  },
+  {
+    image: "/src/assets/images/menu-3.png",
+    title: "tasty and healthy",
+    price: "$15.99",
+    oldPrice: "$20.99",
+    buttonText: "add to cart",
+  },
+  {
+    image: "/src/assets/images/menu-4.png",
+    title: "tasty and healthy",
+    price: "$15.99",
+    oldPrice: "$20.99",
+    buttonText: "add to cart",
+  },
+  {
+    image: "/src/assets/images/menu-5.png",
+    title: "tasty and healthy",
+    price: "$15.99",
+    oldPrice: "$20.99",
+    buttonText: "add to cart",
+  },
+  {
+    image: "/src/assets/images/menu-6.png",
+    title: "tasty and healthy",
+    price: "$15.99",
+    oldPrice: "$20.99",
+    buttonText: "add to cart",
+  },
+];
