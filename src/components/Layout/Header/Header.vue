e46b1b41680b1b4d1b6abe31673dc21ceb6fb29d
FelipeFama <lipehfama@gmail.com>
2024-09-05 23:41:16 -0300

    feat: add new tags and new styles in header component

--- src/components/Layout/Header/Header.vue
+++ src/components/Layout/Header/Header.vue
@@ -13,8 +13,25 @@
       <a href="#contact">contact</a>
       <a href="#blogs">blogs</a>
     </nav>
+    <!-- icons for the navigation -->
+    <nav class="icons">
+      <aside id="search-btn"><IoSearch /></aside>
+      <aside id="cart-btn"><FaCartShopping /></aside>
+      <aside id="menu-btn"><FaBars /></aside>
+    </nav>
+    <!-- search section with its icon -->
+    <aside class="search-form">
+      <input type="search" id="search-box" placeholder="search here..." />
+      <label for="search-box">
+        <IoSearch />
+      </label>
+    </aside>
   </header>
 </template>
+
+<script setup lang="ts">
+import { IoSearch, FaCartShopping, FaBars } from "@kalimahapps/vue-icons";
+</script>
 
 <style lang="scss" scoped>
 @import "./Header.module.scss";
