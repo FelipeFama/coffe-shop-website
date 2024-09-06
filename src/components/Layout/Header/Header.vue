9b7eeeea5403c674308b9a1aa242a64d1cc94fe0
FelipeFama <lipehfama@gmail.com>
2024-09-06 00:36:34 -0300

    feat: menu responsive

--- src/components/Layout/Header/Header.vue
+++ src/components/Layout/Header/Header.vue
@@ -4,7 +4,7 @@
       <img src="@/assets/images/logo.png" alt="logo" />
     </a>
     <!-- navigation bar -->
-    <nav class="navbar">
+    <nav :class="['navbar', { active: isNavbarActive }]">
       <a href="#home">home</a>
       <a href="#about">about</a>
       <a href="#menu">menu</a>
@@ -17,7 +17,7 @@
     <nav class="icons">
       <aside id="search-btn"><IoSearch /></aside>
       <aside id="cart-btn"><FaCartShopping /></aside>
-      <aside id="menu-btn"><FaBars /></aside>
+      <aside id="menu-btn" @click="toggleNavbar"><FaBars /></aside>
     </nav>
     <!-- search section with its icon -->
     <aside class="search-form">
@@ -30,7 +30,14 @@
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
 import { IoSearch, FaCartShopping, FaBars } from "@kalimahapps/vue-icons";
+
+const isNavbarActive = ref(false);
+
+const toggleNavbar = () => {
+  isNavbarActive.value = !isNavbarActive.value;
+};
 </script>
 
 <style lang="scss" scoped>
