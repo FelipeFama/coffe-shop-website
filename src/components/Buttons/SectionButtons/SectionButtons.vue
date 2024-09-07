f278d57e17cf7e00cfbabf2bd6252a482464808d
FelipeFama <lipehfama@gmail.com>
2024-09-07 13:31:45 -0300

    update project

--- src/components/Buttons/SectionButtons/SectionButtons.vue
+++ src/components/Buttons/SectionButtons/SectionButtons.vue
@@ -20,14 +20,6 @@ interface Props {
   isContact?: boolean;
 }
 const props = defineProps<Props>();
-/*
-defineProps({
-  label: {
-    type: String,
-    required: true,
-  },
-});
-*/
 </script>
 
 <style lang="scss" scoped>
